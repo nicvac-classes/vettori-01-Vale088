2a07956edd3d06f0a2676e3c2c2f0f9f851cf307
Vale088 <valerio.sinni@marconibari.edu.it>
2024-12-27 16:44:39 +0000

    esercizio

--- main.cpp
+++ main.cpp
@@ -1,11 +1,14 @@
 #include <iostream>
+#include <vector> 
 using namespace std;
 
-int main() {
-    string nome;
-    cout << "Inserisci il tuo nome: ";
-    cin >> nome;
-    cout << "Ciao " <<nome<< "!" << endl;
-}
+int main(){ 
+     vector<string> cantante(4); 
+      cantante[0]="simba";
+     cantante[1]="lazza";
+     cantante[2]="vasco";
+     cantante[3]="tony";
+     cantante[4]="rondodasosa";
+      }
 
 //LEGGERE LE ISTRUZIONI NEL FILE README.md
